f3416c014fb981eedc627043d3d570cceacac3e0
Divyen <dalwadiddj2000@gmail.com>
2024-07-17 20:28:42 +0530

    Print The Prime Number Series Between The Given Range

---
+++ PrimeNumberSeries.java
@@ -0,0 +1,13 @@
+
+/**
+ * PrimeNumberSeries
+ */
+import java.util.Scanner;
+
+public class PrimeNumberSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Value For The Range n : ");
+        int n = sc.nextInt();
+    }
+}
